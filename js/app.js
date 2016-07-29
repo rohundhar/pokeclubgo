8aaf2cdfdb6554c9eedba19ae6f823b5f6dd57f0
rohundhar <rohundhar@gmail.com>
2016-07-29 15:11:45 -0400

    working skeleton

--- js/app.js
+++ js/app.js
@@ -1,34 +1,196 @@
 $(document).ready(function(){
 
-    /*$("#submitpass").click(function(){
+    $('#mysticpass').keypress(function(event){
+        if (event.which == 13){
 
-        submitPass();
-    }); */
-    $("form").submit(function(){
-        console.log("MADE IT HERE");
-        submitPass();
+            console.log("ENTER WAS PRESSED")
+            var request = new XMLHttpRequest();
+            var password = $('#mysticpass').val();
+            $('#mysticpass').val('');
+            console.log("PASSWORD:"+password);
+            request.open("POST","https://pokeclubgo.herokuapp.com/passwordcheck" , true);
+            request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            request.send("password="+password);
+            // These lines of code initialize the httprequest object and send information in order to receive the proper JSON data 
+
+            request.onreadystatechange = function() {
+                console.log("Waiting for password confirmation...");
+                console.log(request.readyState);
+            if (request.readyState == 4){
+                parsedDatapass = JSON.parse(request.responseText);
+                console.log("Response Text: " + parsedDatapass);
+
+
+                if (parsedDatapass.password_status == 'Incorrect'){
+                    $('#mysincorrect').show();
+                }
+                else{
+                    $('#mysincorrect').hide(1000);
+                }
+                }
+            }
+       }
+       else{
+            console.log("WAS NOT THE ENTER KEY")
+       }
+        });
+        $('#valorpass').keypress(function(event){
+                if (event.which == 13){
+
+                    console.log("ENTER WAS PRESSED")
+                    var request = new XMLHttpRequest();
+                    var password = $('#valorpass').val();
+                    //var parsedDatapass;
+                    $('#valorpass').val('');
+                    console.log("PASSWORD:"+password);
+                    request.open("POST","https://pokeclubgo.herokuapp.com/passwordcheck" , true);
+                    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                    request.send("password="+password);
+                    // These lines of code initialize the httprequest object and send information in order to receive the proper JSON data 
+
+                    request.onreadystatechange = function() {
+                        console.log("Waiting for password confirmation...");
+                        console.log(request.readyState);
+                        if (request.readyState == 4){
+                            parsedDatapass = JSON.parse(request.responseText);
+                            console.log("Response Text: " + parsedDatapass);
+
+                            if (parsedDatapass.password_status == 'Incorrect'){
+                                $('#valincorrect').show();
+                            }
+                            else{
+                                $('#valincorrect').hide(1000);
+                            }
+                        }
+
+                    }
+               }
+               else{
+                    console.log("WAS NOT THE ENTER KEY")
+               }
+
+        });
+        $('#instinctpass').keypress(function(event){
+                if (event.which == 13){
+
+                    console.log("ENTER WAS PRESSED")
+                    var request = new XMLHttpRequest();
+                    var password = $('#instinctpass').val();
+                    $('#instinctpass').val('');
+                    console.log("PASSWORD:"+password);
+                    request.open("POST","https://pokeclubgo.herokuapp.com/passwordcheck" , true);
+                    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+                    request.send("password="+password);
+                    // These lines of code initialize the httprequest object and send information in order to receive the proper JSON data 
+
+                    request.onreadystatechange = function() {
+                        console.log("Waiting for password confirmation...");
+                        console.log(request.readyState);
+                    if (request.readyState == 4){
+                        parsedDatapass = JSON.parse(request.responseText);
+                        console.log("Response Text: " + parsedDatapass);
+
+                        
+
+                            if (parsedDatapass.password_status == 'Incorrect'){
+                                $('#instincorrect').show();
+                            }
+                            else{
+                                $('#instincorrect').hide(1000);
+                                console.log("CORRECT PASSWORD. REDIRECT..")
+                                window.location.replace("http://home.pokeclubgo.com");
+
+                            }
+                        }
+                    }
+
+               }
+               else{
+                    console.log("WAS NOT THE ENTER KEY")
+               }
+
+
+        });
+
+
+
+
+
+
+
+
+
+/*
+    $('#mysticsend').click(function(){
+
+        console.log("BUTTON WAS PRESSED")
+        var request = new XMLHttpRequest();
+        var password = $('#mysticpass').val();
+        $('#mysticpass').val('');
+        console.log("PASSWORD:"+password);
+        request.open("POST","https://pokeclubgo.herokuapp.com/passwordcheck" , true);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        request.send("password="+password);
+        // These lines of code initialize the httprequest object and send information in order to receive the proper JSON data 
+
+        request.onreadystatechange = function() {
+            console.log("Waiting for password confirmation...");
+            console.log(request.readyState);
+        if (request.readyState == 4){
+            parsedDatapass = JSON.parse(request.responseText);
+            console.log("Response Text: " + parsedDatapass);
+
+            }
+        }
+
     });
+    $('#valorsend').click(function(){
+
+        console.log("BUTTON WAS PRESSED")
+        var request = new XMLHttpRequest();
+        var password = $('#valorpass').val();
+        $('#valorpass').val('');
+        console.log("PASSWORD:"+password);
+        request.open("POST","https://pokeclubgo.herokuapp.com/passwordcheck" , true);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        request.send("password="+password);
+        // These lines of code initialize the httprequest object and send information in order to receive the proper JSON data 
+
+        request.onreadystatechange = function() {
+            console.log("Waiting for password confirmation...");
+            console.log(request.readyState);
+        if (request.readyState == 4){
+            parsedDatapass = JSON.parse(request.responseText);
+            console.log("Response Text: " + parsedDatapass);
+
+            }
+        }
+
+    });
+    $('#instinctsend').click(function(){
+
+        console.log("BUTTON WAS PRESSED")
+        var request = new XMLHttpRequest();
+        var password = $('#instinctpass').val();
+        $('#instinctpass').val('');
+        console.log("PASSWORD:"+password);
+        request.open("POST","https://pokeclubgo.herokuapp.com/passwordcheck" , true);
+        request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        request.send("password="+password);
+        // These lines of code initialize the httprequest object and send information in order to receive the proper JSON data 
+
+        request.onreadystatechange = function() {
+            console.log("Waiting for password confirmation...");
+            console.log(request.readyState);
+        if (request.readyState == 4){
+            parsedDatapass = JSON.parse(request.responseText);
+            console.log("Response Text: " + parsedDatapass);
+
+            }
+        }
+
+    }); */
 
 });
 
 
-function submitPass(){
-
-    var request = new XMLHttpRequest();
-    var password = $('#masterpassword').val();
-    request.open("POST","https://pokeclubgo.herokuapp.com/passwordcheck" , true);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    request.send("password="+password);
-    // These lines of code initialize the httprequest object and send information in order to receive the proper JSON data 
-
-    request.onreadystatechange = function() {
-        console.log("Waiting for password confirmation...");
-        console.log(request.readyState);
-    if (request.readyState == 4){
-        parsedData = JSON.parse(request.responseText);
-        console.log("Response Text: " + parsedData);
-
-        }
-    }
-
-}
